e61c6cfe6896e6f1cf3d7b577843173bfae5028a
Osama Riaz <riazosama@gmail.com>
2024-09-18 15:08:00 +0500

    convert login to latest syntax.

--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -1,11 +1,18 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, inject, signal } from "@angular/core";
 import { Router } from "@angular/router";
-import { Store } from "@ngrx/store";
 import { AuthService } from "src/app/auth.service";
 import { LayoutService } from "src/app/layout/service/app.layout.service";
+import { Store } from "@ngrx/store";
 import { add } from "src/app/store/actions/user.action";
 import { selectAccessToken } from "src/app/store/selectors/user.selector";
 import { CustomJwtPayload } from "src/app/token-payload.model";
+import { toSignal } from "@angular/core/rxjs-interop"; // Import helper for signal conversion
+import { FormsModule } from "@angular/forms";
+import { ButtonModule } from "primeng/button";
+import { CheckboxModule } from "primeng/checkbox";
+import { InputTextModule } from "primeng/inputtext";
+import { PasswordModule } from "primeng/password";
+import { catchError, map, of } from "rxjs";
 
 @Component({
   selector: "app-login",
@@ -20,55 +27,59 @@ import { CustomJwtPayload } from "src/app/token-payload.model";
       }
     `,
   ],
+  imports: [FormsModule, ButtonModule, CheckboxModule, InputTextModule, PasswordModule],
+  standalone: true,
+  providers: [LayoutService, AuthService, Store],
 })
-export class LoginComponent implements OnInit {
-  valCheck: string[] = ["remember"];
+export class LoginComponent {
+  valCheck = ["remember"];
+  password = signal<string>("");
+  email = signal<string>("");
 
-  password!: string;
+  private authService = inject(AuthService);
+  private router = inject(Router);
+  private store = inject(Store);
 
-  email: string;
+  accessToken = toSignal(this.store.select(selectAccessToken));
 
-  constructor(public layoutService: LayoutService , private authService: AuthService , private router:Router , private store:Store , ) {}
-
-
-  ngOnInit(): void {
-    this.store.select(selectAccessToken).subscribe((accessToken) => {
-      if (accessToken) {
-        this.router.navigate(['']);
-      }
-    });
+  ngOnInit() {
+    if (this.accessToken()) {
+      this.router.navigate([""]);
+    }
   }
 
+  submit() {
+    this.authService
+      .login(this.email(), this.password())
+      .pipe(
+        map((response) => {
+          const accessToken = response.access_token;
+          const decodedToken: CustomJwtPayload | null = this.authService.decodeToken(accessToken);
 
-  Submit(){
-    this.authService.login(this.email, this.password).subscribe(
-      (response) => {
+          this.store.dispatch(
+            add({
+              user: {
+                username: decodedToken?.preferred_username || this.email(),
+                accessToken: accessToken,
+                refreshToken: response.refresh_token,
+                email: decodedToken?.email,
+                email_verified: decodedToken?.email_verified,
+                realm_access: decodedToken?.realm_access,
+                resource_access: decodedToken?.resource_access,
+                scope: decodedToken?.scope,
+                session_state: decodedToken?.session_state,
+                sid: decodedToken?.sid,
+              },
+            }),
+          );
 
-        const accessToken = response.access_token;
-        const decodedToken: CustomJwtPayload | null = this.authService.decodeToken(accessToken);
-
-          this.store.dispatch(add({
-            user: {
-              username: decodedToken.preferred_username || this.email,
-              accessToken: accessToken,
-              refreshToken: response.refresh_token,
-              email: decodedToken.email,
-              email_verified: decodedToken.email_verified,
-              realm_access: decodedToken.realm_access,
-              resource_access: decodedToken.resource_access,
-              scope: decodedToken.scope,
-              session_state: decodedToken.session_state,
-              sid: decodedToken.sid,
-            }
-          }));
-
-          this.router.navigate([''])
-      },
-      (error) => {
-        console.error('Login failed', error);
-        alert("Invalid Credentials")
-      }
-    );
+          this.router.navigate([""]);
+        }),
+        catchError(() => {
+          alert("Invalid Credentials");
+          return of(null);
+        }),
+      )
+      .subscribe();
   }
-
 }
