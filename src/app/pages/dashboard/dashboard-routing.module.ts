60f1a7dbc35a115cae7d2be3e5e8f8c737613874
Farhan <farhaan.ranjha@gmail.com>
2024-09-23 12:30:57 +0500

    fixed file name

--- src/app/pages/dashboard/dashboard-routing.module.ts
+++ src/app/pages/dashboard/dashboard-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
 import { DashboardComponent } from "./dashboard.component";
 import { AddPatientComponent } from "../add-patient/add-patient.component";
-import { PendingEnrollmentsComponent } from "../pendingenrollments/pendingenrollments.component";
+import { PendingEnrollmentsComponent } from "../pending-enrollments/pending-enrollments.component";
 import { PendingReadingsComponent } from "../pending-readings/pending-readings.component";
 import { ReadingsNotAddressedComponent } from "../readings-not-addressed/readings-not-addressed.component";
 
