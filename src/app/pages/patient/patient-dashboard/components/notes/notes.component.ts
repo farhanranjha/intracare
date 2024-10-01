45e0847a8c9281d5a98e15acd514940723f66932
Farhan <farhaan.ranjha@gmail.com>
2024-10-01 18:21:01 +0500

    added notes to seperate comp

--- src/app/pages/patient/patient-dashboard/components/notes/notes.component.ts
+++ src/app/pages/patient/patient-dashboard/components/notes/notes.component.ts
@@ -1,7 +1,7 @@
 import { Component } from "@angular/core";
 
 @Component({
-  selector: "app-notes",
+  selector: "patient-dashboard-notes",
   standalone: true,
   imports: [],
   templateUrl: "./notes.component.html",
--- src/app/pages/patient/patient-dashboard/components/patient-info-tab/patient-info-tab.component.ts
+++ src/app/pages/patient/patient-dashboard/components/patient-info-tab/patient-info-tab.component.ts
@@ -1,11 +1,12 @@
 import { Component } from "@angular/core";
 import { ButtonModule } from "primeng/button";
 import { ProgressBarModule } from "primeng/progressbar";
+import { NotesComponent } from "../notes/notes.component";
 
 @Component({
   selector: "patient-dashboard-info",
   standalone: true,
-  imports: [ProgressBarModule, ButtonModule],
+  imports: [ProgressBarModule, ButtonModule, NotesComponent],
   templateUrl: "./patient-info-tab.component.html",
   styleUrl: "./patient-info-tab.component.scss",
 })
