d6df7e647ce7df8eb6e25eb81bb8ae8242ad2059
Farhan <farhaan.ranjha@gmail.com>
2024-10-01 19:24:11 +0500

    added second level of router

--- src/app/pages/patient/patient-dashboard/components/general-settings/general-settings.component.ts
+++ src/app/pages/patient/patient-dashboard/components/general-settings/general-settings.component.ts
@@ -1,11 +1,19 @@
 import { Component } from "@angular/core";
 import { RouterModule } from "@angular/router";
+import { TabRouterComponent } from "src/app/components/tab-router/tab-router.component";
+import { IKeyValue } from "src/app/types/common-types";
 
 @Component({
   selector: "app-general-settings",
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, TabRouterComponent],
   templateUrl: "./general-settings.component.html",
   styleUrl: "./general-settings.component.scss",
 })
-export class GeneralSettingsComponent {}
+export class GeneralSettingsComponent {
+  routes: IKeyValue[] = [
+    { label: "Profile", value: "profile" },
+    { label: "Devices", value: "devices" },
+    { label: "Custom Devices Alert", value: "custom-vital-alerts" },
+  ];
+}
