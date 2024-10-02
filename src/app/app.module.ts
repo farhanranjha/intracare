0243290bf709a00ce6b098ce81aa17b23ced8e72
Farhan <farhaan.ranjha@gmail.com>
2024-10-02 15:18:46 +0500

    added auth and error interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,26 @@ import { AppComponent } from "./app.component";
 import { AppLayoutModule } from "./layout/app.layout.module";
 import { NotfoundComponent } from "./pages/notfound/notfound.component";
 import { userReducer } from "./store/reducers/user.reducer";
-import { HttpClientModule } from "@angular/common/http";
+import { HTTP_INTERCEPTORS, HttpClientModule } from "@angular/common/http";
+import { AuthInterceptor } from "./interceptors/auth.interceptor";
+import { ErrorInterceptor } from "./interceptors/error.interceptor";
 
 @NgModule({
   declarations: [AppComponent, NotfoundComponent],
   imports: [HttpClientModule, AppRoutingModule, AppLayoutModule, StoreModule.forRoot({ user: userReducer })],
-  providers: [{ provide: LocationStrategy, useClass: PathLocationStrategy }],
+  providers: [
+    { provide: LocationStrategy, useClass: PathLocationStrategy },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true,
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: ErrorInterceptor,
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/services/dashboard/dashboard.service.ts
+++ src/app/services/dashboard/dashboard.service.ts
@@ -1,5 +1,5 @@
+import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { map } from "rxjs/operators";
 
@@ -66,13 +66,8 @@ export class DashboardService {
   private issuerUri = import.meta.env.NG_APP_KEYCLOAK_BASE_URL;
 
   getDashboardData(payload): Observable<{ data; total: number }> {
-    const headers = new HttpHeaders({
-      Authorization: `Bearer eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJQZThRek5lSlVTS2t3WHlValF4LTNEc3lSWlM1ZkJjU0d6NktIMVNsZVNzIn0.eyJleHAiOjE3Mjc4NjM1MDgsImlhdCI6MTcyNzg2MzIwOCwianRpIjoiZThlYjVjYmEtZmMzNS00NDkzLWI5NjUtNTNlOGE0NmM2ZTE4IiwiaXNzIjoiaHR0cHM6Ly9jcmgtYXlkbC5jb20vcmVhbG1zL0RFVkludHJhQ2FyZSIsImF1ZCI6ImFjY291bnQiLCJzdWIiOiJlMzhiNDk5OC00Yzc0LTQyOGUtODgyMy04ZWQ2M2JlNTAyNzAiLCJ0eXAiOiJCZWFyZXIiLCJhenAiOiJycG0tY2xpZW50LWRldiIsInNlc3Npb25fc3RhdGUiOiI4ZWNmYzViMy04YzhkLTQ4ZDItODllZC03MTZmNDYzZjFlNTgiLCJhY3IiOiIxIiwiYWxsb3dlZC1vcmlnaW5zIjpbIioiXSwicmVhbG1fYWNjZXNzIjp7InJvbGVzIjpbIm9mZmxpbmVfYWNjZXNzIiwidW1hX2F1dGhvcml6YXRpb24iLCJkZWZhdWx0LXJvbGVzLWRldmludHJhY2FyZSJdfSwicmVzb3VyY2VfYWNjZXNzIjp7ImFjY291bnQiOnsicm9sZXMiOlsibWFuYWdlLWFjY291bnQiLCJtYW5hZ2UtYWNjb3VudC1saW5rcyIsInZpZXctcHJvZmlsZSJdfX0sInNjb3BlIjoiZW1haWwgcHJvZmlsZSIsInNpZCI6IjhlY2ZjNWIzLThjOGQtNDhkMi04OWVkLTcxNmY0NjNmMWU1OCIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJuYW1lIjoiRmFyaGFuIFJhbmpoYSIsInByZWZlcnJlZF91c2VybmFtZSI6ImZhcmhhbi5yYW5qaGFAcmlwZXNlZWQuaW8iLCJnaXZlbl9uYW1lIjoiRmFyaGFuIiwiZmFtaWx5X25hbWUiOiJSYW5qaGEiLCJlbWFpbCI6ImZhcmhhbi5yYW5qaGFAcmlwZXNlZWQuaW8ifQ.gAn3Dl1byu7hVo5Yz6eQWuZ-fNMNwLetdmUkcv_uQBpXR1NPbIHu78r6AGwhUFfxBJAHmgz2YekRMwxGsoRi2M8W-Cb6j3CSI2TLfvqCx7sB9eRqxLG9ETH-TihNYSG-zqBy3MEC6thuEHIqjcql2pQCRj9w2dCB5AiWN8HOJ519O5JJf0LGUGGtzMWouyLfw1LQ3i9SVBjibRxiLsPERXDLH0keP64ig3RaqVBHyAGy6-tHTczrLBWOqzYYhNdliPAtYMBj6D6MhE6QJkIdIJn_W3ORdqOKZzH0guJ4c1lRs5VpFLSh8arZsMGdLxDyjd6K_IQpx5zZSZz-Z2xT9Q`,
-    });
-    return this.http.post<any>(`${this.issuerUri}/data/filter`, payload, { headers }).pipe(
+    return this.http.post<any>(`${this.issuerUri}/data/filter`, payload).pipe(
       map((data) => {
-        console.log("===data===> ", data);
-
         return { data, total: 50 };
       }),
     );
