4ba27950b60939b80138c122ceed72321a90932c
Osama Riaz <riazosama@gmail.com>
2024-10-07 18:30:20 +0500

    styles updated for task

--- src/app/pages/patient/patient-dashboard/components/task-sidebar/task-sidebar.component.ts
+++ src/app/pages/patient/patient-dashboard/components/task-sidebar/task-sidebar.component.ts
@@ -5,11 +5,12 @@ import { DropdownModule } from "primeng/dropdown";
 import { CalendarModule } from "primeng/calendar";
 import { CheckboxModule } from "primeng/checkbox";
 import { ButtonModule } from "primeng/button";
+import { DividerModule } from "primeng/divider";
 
 @Component({
   selector: "task-sidebar",
   standalone: true,
-  imports: [SidebarModule, InputTextareaModule, DropdownModule, CalendarModule, CheckboxModule, ButtonModule],
+  imports: [SidebarModule, InputTextareaModule, DropdownModule, CalendarModule, CheckboxModule, ButtonModule, DividerModule],
   templateUrl: "./task-sidebar.component.html",
   styleUrl: "./task-sidebar.component.scss",
 })
