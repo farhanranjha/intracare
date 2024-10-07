fa6943e9a047a1bda4653fb0a4b6eb328ed7eed6
Usman-Ahmad673 <itsusmanahmad00@gmail.com>
2024-10-07 18:23:13 +0500

    Side bar is opning on clicking view all for notes

---
+++ src/app/services/sidebar/sidebar.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from "@angular/core";
+import { BehaviorSubject } from "rxjs";
+
+@Injectable({
+  providedIn: "root",
+})
+export class SidebarService {
+  private sidebarVisibleSource = new BehaviorSubject<boolean>(false);
+  sidebarVisible$ = this.sidebarVisibleSource.asObservable();
+
+  toggleSidebar() {
+    this.sidebarVisibleSource.next(!this.sidebarVisibleSource.value);
+  }
+
+  setSidebarVisible(isVisible: boolean) {
+    this.sidebarVisibleSource.next(isVisible);
+  }
+}
