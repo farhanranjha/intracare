f954d8ec35628be4c60149a7e2f26245c7306469
Farhan <farhaan.ranjha@gmail.com>
2024-10-08 16:31:53 +0500

    made messages side bar

--- src/app/pages/patient/patient-dashboard/components/messages-sidebar/messages-sidebar.component.ts
+++ src/app/pages/patient/patient-dashboard/components/messages-sidebar/messages-sidebar.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from "@angular/common";
 import { Component, Input } from "@angular/core";
 import { ButtonModule } from "primeng/button";
 import { DividerModule } from "primeng/divider";
@@ -7,10 +8,26 @@ import { SidebarModule } from "primeng/sidebar";
 @Component({
   selector: "messages-sidebar",
   standalone: true,
-  imports: [SidebarModule, InputTextareaModule, ButtonModule, DividerModule],
+  imports: [SidebarModule, InputTextareaModule, ButtonModule, DividerModule, CommonModule],
   templateUrl: "./messages-sidebar.component.html",
   styleUrl: "./messages-sidebar.component.scss",
 })
 export class MessagesSidebarComponent {
   @Input() sidebarVisible: boolean = false;
+  mockMsgs = [
+    {
+      message:
+        "Let's meet for lunch tomorrow and discuss the project updates in detail. I received your email and will review the attached files by the end of the day. I received your email and will review the attached files by the end of the day.",
+      date: "11/12/2025",
+    },
+    {
+      message: "Can you send me the final version of the document before the deadline tomorrow?",
+      date: "11/12/2025",
+    },
+    {
+      message:
+        "I received your email and will review the attached files by the end of the day. Can you send me the final version of the document before the deadline tomorrow?",
+      date: "11/12/2025",
+    },
+  ];
 }
