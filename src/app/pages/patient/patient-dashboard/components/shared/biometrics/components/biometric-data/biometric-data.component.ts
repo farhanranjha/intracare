a8152753d462349b247702fa7302a2375217b24a
Farhan <farhaan.ranjha@gmail.com>
2024-10-08 17:22:18 +0500

    updated cards design according to figma

--- src/app/pages/patient/patient-dashboard/components/shared/biometrics/components/biometric-data/biometric-data.component.ts
+++ src/app/pages/patient/patient-dashboard/components/shared/biometrics/components/biometric-data/biometric-data.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from "@angular/common";
 import { Component } from "@angular/core";
+import { DividerModule } from "primeng/divider";
 import { biometricsMockData } from "src/app/utils/constants/mock-data";
 
 @Component({
   selector: "biometric-data",
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, DividerModule],
   templateUrl: "./biometric-data.component.html",
   styleUrl: "./biometric-data.component.scss",
 })
