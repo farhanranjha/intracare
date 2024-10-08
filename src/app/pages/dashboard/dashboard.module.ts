cc7339e4412008585257be19f541424a6037daa9
Farhan <farhaan.ranjha@gmail.com>
2024-10-08 11:18:21 +0500

    removed old notes component

--- src/app/pages/dashboard/dashboard.module.ts
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -1,13 +1,12 @@
+import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
+import { ButtonModule } from "primeng/button";
+import { ProgressBarModule } from "primeng/progressbar";
+import { DashboardService } from "src/app/services/dashboard/dashboard.service";
+import { CustomFilterComponent } from "../../components/table/custom-filter/custom-filter.component";
 import { TableComponent } from "../../components/table/table.component";
 import { DashboardsRoutingModule } from "./dashboard-routing.module";
 import { DashboardComponent } from "./dashboard.component";
-import { ButtonModule } from "primeng/button";
-import { CustomFilterComponent } from "../../components/table/custom-filter/custom-filter.component";
-import { PatientNotesComponent } from "src/app/components/Notes/patient-notes/patient-notes.component";
-import { DashboardService } from "src/app/services/dashboard/dashboard.service";
-import { CommonModule } from "@angular/common";
-import { ProgressBarModule } from "primeng/progressbar";
 
 @NgModule({
   imports: [
@@ -16,7 +15,6 @@ import { ProgressBarModule } from "primeng/progressbar";
     TableComponent,
     ButtonModule,
     CustomFilterComponent,
-    PatientNotesComponent,
     ProgressBarModule,
   ],
   declarations: [DashboardComponent],
