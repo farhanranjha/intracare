af40ebf7fb6005118fef120c252cb28867915668
Farhan <farhaan.ranjha@gmail.com>
2024-10-08 15:23:34 +0500

    added mock data to dedicated file

--- src/app/pages/patient/patient-dashboard/components/shared/biometrics/components/biometric-data/biometric-data.component.ts
+++ src/app/pages/patient/patient-dashboard/components/shared/biometrics/components/biometric-data/biometric-data.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from "@angular/common";
 import { Component } from "@angular/core";
+import { biometricsMockData } from "src/app/utils/constants/mock-data";
 
 @Component({
   selector: "biometric-data",
@@ -9,48 +10,5 @@ import { Component } from "@angular/core";
   styleUrl: "./biometric-data.component.scss",
 })
 export class BiometricDataComponent {
-  mockData = [
-    {
-      metricName: "Blood Glucose",
-      value: "133",
-      metricUnit: "mg/DL",
-      icon: "assets/layout/styles/icons/glucometer.png",
-    },
-    {
-      metricName: "Blood Pressure",
-      value: "133",
-      metricUnit: "mmHg",
-      icon: "assets/layout/styles/icons/blood-pressure.png",
-    },
-    {
-      metricName: "Weight",
-      value: "133",
-      metricUnit: "lbs",
-      icon: "assets/layout/styles/icons/weight-scales.png",
-    },
-    {
-      metricName: "Oxygen Saturation",
-      value: "133",
-      metricUnit: "SpO2",
-      icon: "assets/layout/styles/icons/oxygen-saturation.png",
-    },
-    {
-      metricName: "Heart Rate",
-      value: "133",
-      metricUnit: "bpm",
-      icon: "assets/layout/styles/icons/heart-rate.png",
-    },
-    {
-      metricName: "Cholestrol",
-      value: "133",
-      metricUnit: "mg/DL",
-      icon: "assets/layout/styles/icons/cholesterol.png",
-    },
-    {
-      metricName: "Temprature",
-      value: "133",
-      metricUnit: "Farenheit",
-      icon: "assets/layout/styles/icons/thermometer.png",
-    },
-  ];
+  mockData = biometricsMockData;
 }
--- src/app/utils/constants/mock-data.ts
+++ src/app/utils/constants/mock-data.ts
@@ -808,3 +808,47 @@ export const timerLogsData = [
     doctorName: "Dr. Wilson",
   },
 ];
+export const biometricsMockData = [
+  {
+    metricName: "Blood Glucose",
+    value: "133",
+    metricUnit: "mg/DL",
+    icon: "assets/layout/styles/icons/glucometer.png",
+  },
+  {
+    metricName: "Blood Pressure",
+    value: "133",
+    metricUnit: "mmHg",
+    icon: "assets/layout/styles/icons/blood-pressure.png",
+  },
+  {
+    metricName: "Weight",
+    value: "133",
+    metricUnit: "lbs",
+    icon: "assets/layout/styles/icons/weight-scales.png",
+  },
+  {
+    metricName: "Oxygen Saturation",
+    value: "133",
+    metricUnit: "SpO2",
+    icon: "assets/layout/styles/icons/oxygen-saturation.png",
+  },
+  {
+    metricName: "Heart Rate",
+    value: "133",
+    metricUnit: "bpm",
+    icon: "assets/layout/styles/icons/heart-rate.png",
+  },
+  {
+    metricName: "Cholestrol",
+    value: "133",
+    metricUnit: "mg/DL",
+    icon: "assets/layout/styles/icons/cholesterol.png",
+  },
+  {
+    metricName: "Temprature",
+    value: "133",
+    metricUnit: "Farenheit",
+    icon: "assets/layout/styles/icons/thermometer.png",
+  },
+];
