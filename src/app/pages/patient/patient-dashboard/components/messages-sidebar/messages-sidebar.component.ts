1d9d20201d1fbd879e55a37a7fe787ef6decc4f1
Farhan <farhaan.ranjha@gmail.com>
2024-10-08 17:30:59 +0500

    made messages dynamic

--- src/app/pages/patient/patient-dashboard/components/messages-sidebar/messages-sidebar.component.ts
+++ src/app/pages/patient/patient-dashboard/components/messages-sidebar/messages-sidebar.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from "@angular/common";
 import { Component, Input } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 import { ButtonModule } from "primeng/button";
 import { DividerModule } from "primeng/divider";
 import { InputTextareaModule } from "primeng/inputtextarea";
@@ -8,26 +9,28 @@ import { SidebarModule } from "primeng/sidebar";
 @Component({
   selector: "messages-sidebar",
   standalone: true,
-  imports: [SidebarModule, InputTextareaModule, ButtonModule, DividerModule, CommonModule],
+  imports: [SidebarModule, InputTextareaModule, ButtonModule, DividerModule, CommonModule, FormsModule],
   templateUrl: "./messages-sidebar.component.html",
   styleUrl: "./messages-sidebar.component.scss",
 })
 export class MessagesSidebarComponent {
   @Input() sidebarVisible: boolean = false;
+  newMessage: string = "";
+
   mockMsgs = [
-    {
-      message:
-        "Let's meet for lunch tomorrow and discuss the project updates in detail. I received your email and will review the attached files by the end of the day. I received your email and will review the attached files by the end of the day.",
-      date: "11/12/2025",
-    },
     {
       message: "Can you send me the final version of the document before the deadline tomorrow?",
       date: "11/12/2025",
     },
-    {
-      message:
-        "I received your email and will review the attached files by the end of the day. Can you send me the final version of the document before the deadline tomorrow?",
-      date: "11/12/2025",
-    },
   ];
+
+  sendMessage() {
+    if (this.newMessage.trim()) {
+      this.mockMsgs.push({
+        message: this.newMessage,
+        date: new Date().toLocaleDateString(),
+      });
+      this.newMessage = "";
+    }
+  }
 }
