032b2111c94a795d56607e90080e7e914b37637b
Farhan <farhaan.ranjha@gmail.com>
2024-10-09 15:17:52 +0500

    added trends component and did routing

---
+++ src/app/pages/patient/patient-dashboard/components/shared/biometrics/components/trends/trends.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "current-trends",
+  standalone: true,
+  imports: [],
+  templateUrl: "./trends.component.html",
+  styleUrl: "./trends.component.scss",
+})
+export class TrendsComponent {}
