13c7e3b730d2071348dbbed9f4307f57e2cb9c35
Farhan <farhaan.ranjha@gmail.com>
2024-10-10 14:39:33 +0500

    initial done

--- src/app/pages/patient/patient-dashboard/components/ccm/components/screenings/screenings.component.ts
+++ src/app/pages/patient/patient-dashboard/components/ccm/components/screenings/screenings.component.ts
@@ -1,12 +1,50 @@
-import { Component } from '@angular/core';
+import { CommonModule } from "@angular/common";
+import { Component } from "@angular/core";
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from "@angular/forms";
+import { AccordionModule } from "primeng/accordion";
+import { ButtonModule } from "primeng/button";
+import { CheckboxModule } from "primeng/checkbox";
+import { InputTextModule } from "primeng/inputtext";
+import { screeningSchema } from "src/app/utils/constants/mock-data";
 
 @Component({
-  selector: 'app-screenings',
+  selector: "app-screenings",
   standalone: true,
-  imports: [],
-  templateUrl: './screenings.component.html',
-  styleUrl: './screenings.component.scss'
+  imports: [ReactiveFormsModule, AccordionModule, CheckboxModule, InputTextModule, ButtonModule, CommonModule],
+  templateUrl: "./screenings.component.html",
+  styleUrl: "./screenings.component.scss",
 })
 export class ScreeningsComponent {
+  screeningForm: FormGroup = this.fb.group({});
+  schema = screeningSchema;
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.buildForm();
+  }
+
+  buildForm() {
+    this.schema.forEach((category) => {
+      const categoryGroup = this.fb.group({});
+      category.questions.forEach((question) => {
+        const questionGroup = this.fb.group({});
+        question.responses.forEach((response) => {
+          let control;
+          if (response.type === "checkbox") {
+            control = this.fb.control(false, response.required ? Validators.required : null);
+          } else {
+            control = this.fb.control("", response.required ? Validators.required : null);
+          }
+          questionGroup.addControl(response.name, control);
+        });
+        categoryGroup.addControl(`question_${question.questionId}`, questionGroup);
+      });
+      this.screeningForm.addControl(`category_${category.categoryId}`, categoryGroup);
+    });
+  }
+
+  onSubmit() {
+    console.log(this.screeningForm.value);
+  }
 }
