b7ea8b9b841377d909d9bee757ce51b6a20d4108
Usman-Ahmad673 <itsusmanahmad00@gmail.com>
2024-10-11 17:34:06 +0500

    merged development

--- src/app/pages/patient/patient-dashboard/components/notes/notes.component.ts
+++ src/app/pages/patient/patient-dashboard/components/notes/notes.component.ts
@@ -3,24 +3,23 @@ import { DividerModule } from "primeng/divider";
 import { SidebarService } from "src/app/services/sidebar/sidebar.service";
 import { NotesSidebarComponent } from "../notes-sidebar/notes-sidebar.component";
 import { TasksComponent } from "../tasks/tasks.component";
+import { ButtonModule } from "primeng/button";
+import { TaskSidebarComponent } from "../task-sidebar/task-sidebar.component";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "patient-dashboard-notes",
   standalone: true,
-  imports: [DividerModule, TasksComponent, NotesSidebarComponent],
+  imports: [DividerModule, TasksComponent, NotesSidebarComponent, ButtonModule],
   templateUrl: "./notes.component.html",
-  styleUrls: ["./notes.component.scss"],
+  styleUrl: "./notes.component.scss",
 })
 export class NotesComponent {
   isExpanded: boolean = false;
   noteContent: string =
     "Lorem ipsum dolor sit , consectetur Duis sollicitudin sollicitudin laoreet pulvinar sollicitudin laoreet hah eet Lorem ipsum dolor sit , consectetur Duis sollicitudin sollicitudin laoreet pulvinar sollicitudin laoreet hah eet Lorem ipsum dolor sit , consectetur Duis sollicitudin sollicitudin laoreet pulvinar sollicitudin laoreet hah eet.Lorem ipsum dolor sit , consectetur Duis sollicitudin sollicitudin laoreet pulvinar sollicitudin laoreet hah eet Lorem ipsum dolor sit , consectetur Duis sollicitudin sollicitudin laoreet pulvinar sollicitudin laoreet hah eet Lorem ipsum dolor sit , consectetur Duis sollicitudin sollicitudin laoreet pulvinar sollicitudin laoreet hah eet.Lorem ipsum dolor sit , consectetur Duis sollicitudin sollicitudin laoreet pulvinar sollicitudin laoreet hah eet Lorem ipsum dolor sit , consectetur Duis sollicitudin sollicitudin laoreet pulvinar sollicitudin laoreet hah eet Lorem ipsum dolor sit , consectetur Duis sollicitudin sollicitudin laoreet pulvinar sollicitudin laoreet hah eet.Lorem ipsum dolor sit , consectetur Duis sollicitudin sollicitudin laoreet pulvinar sollicitudin laoreet hah eet Lorem ipsum dolor sit , consectetur Duis sollicitudin sollicitudin laoreet pulvinar sollicitudin laoreet hah eet Lorem ipsum dolor sit , consectetur Duis sollicitudin sollicitudin laoreet pulvinar sollicitudin laoreet hah eet.Lorem ipsum dolor sit , consectetur Duis sollicitudin sollicitudin laoreet pulvinar sollicitudin laoreet hah eet Lorem ipsum dolor sit , consectetur Duis sollicitudin sollicitudin laoreet pulvinar sollicitudin laoreet hah eet Lorem ipsum dolor sit , consectetur Duis sollicitudin sollicitudin laoreet pulvinar sollicitudin laoreet hah eet";
 
-  constructor(private sidebarService: SidebarService) {}
-
-  viewAllNotes() {
-    this.sidebarService.setSidebarVisible(true);
-  }
+  constructor(private router: Router) {}
 
   toggleExpand() {
     this.isExpanded = !this.isExpanded;
@@ -28,5 +27,8 @@ export class NotesComponent {
 
   getTruncatedText(text: string): string {
     return this.isExpanded ? text : text.slice(0, 100) + "...";
+  }
+  gotToNotesSection() {
+    this.router.navigate(["/rpm/notes"]);
   }
 }
--- src/app/pages/patient/patient-dashboard/components/tasks/tasks.component.ts
+++ src/app/pages/patient/patient-dashboard/components/tasks/tasks.component.ts
@@ -1,14 +1,18 @@
 import { Component } from "@angular/core";
+import { Router } from "@angular/router";
+import { ButtonModule } from "primeng/button";
 import { DividerModule } from "primeng/divider";
+import { TaskSidebarComponent } from "../task-sidebar/task-sidebar.component";
 
 @Component({
   selector: "app-tasks",
   standalone: true,
-  imports: [DividerModule],
+  imports: [DividerModule, TaskSidebarComponent, ButtonModule],
   templateUrl: "./tasks.component.html",
   styleUrl: "./tasks.component.scss",
 })
 export class TasksComponent {
+  constructor(private router: Router) {}
   isExpanded: boolean = false;
   noteContent: string =
     "Lorem ipsum dolor sit , consectetur Duis sollicitudin sollicitudin laoreet pulvinar sollicitudin laoreet hah eet Lorem ipsum dolor sit , consectetur Duis sollicitudin sollicitudin laoreet pulvinar sollicitudin laoreet hah eet Lorem ipsum dolor sit , consectetur Duis sollicitudin sollicitudin laoreet pulvinar sollicitudin laoreet hah eet.Lorem ipsum dolor sit , consectetur Duis sollicitudin sollicitudin laoreet pulvinar sollicitudin laoreet hah eet Lorem ipsum dolor sit , consectetur Duis sollicitudin sollicitudin laoreet pulvinar sollicitudin laoreet hah eet Lorem ipsum dolor sit , consectetur Duis sollicitudin sollicitudin laoreet pulvinar sollicitudin laoreet hah eet.Lorem ipsum dolor sit , consectetur Duis sollicitudin sollicitudin laoreet pulvinar sollicitudin laoreet hah eet Lorem ipsum dolor sit , consectetur Duis sollicitudin sollicitudin laoreet pulvinar sollicitudin laoreet hah eet Lorem ipsum dolor sit , consectetur Duis sollicitudin sollicitudin laoreet pulvinar sollicitudin laoreet hah eet.Lorem ipsum dolor sit , consectetur Duis sollicitudin sollicitudin laoreet pulvinar sollicitudin laoreet hah eet Lorem ipsum dolor sit , consectetur Duis sollicitudin sollicitudin laoreet pulvinar sollicitudin laoreet hah eet Lorem ipsum dolor sit , consectetur Duis sollicitudin sollicitudin laoreet pulvinar sollicitudin laoreet hah eet.Lorem ipsum dolor sit , consectetur Duis sollicitudin sollicitudin laoreet pulvinar sollicitudin laoreet hah eet Lorem ipsum dolor sit , consectetur Duis sollicitudin sollicitudin laoreet pulvinar sollicitudin laoreet hah eet Lorem ipsum dolor sit , consectetur Duis sollicitudin sollicitudin laoreet pulvinar sollicitudin laoreet hah eet";
@@ -19,5 +23,19 @@ export class TasksComponent {
 
   getTruncatedText(text: string): string {
     return this.isExpanded ? text : text.slice(0, 100) + "...";
+  }
+  taskBarVisible: boolean = false;
+
+  gotToNotesSection() {
+    // this.router.navigate(["/patient/1/rpm/notes"]);
+    this.router.navigateByUrl("/patient/1/rpm/notes");
+  }
+
+  openTaskSidebar() {
+    this.taskBarVisible = true;
+  }
+
+  onTaskSidebarClose(value: boolean) {
+    this.taskBarVisible = value;
   }
 }
