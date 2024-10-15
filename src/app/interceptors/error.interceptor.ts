11195b89f810ccf065400d18ba73bd39871f7010
Farhan <farhaan.ranjha@gmail.com>
2024-10-15 15:55:06 +0500

    [INTRA-69]: fixed infinite api calls after refresh token

--- src/app/interceptors/error.interceptor.ts
+++ src/app/interceptors/error.interceptor.ts
@@ -20,6 +20,10 @@ export class ErrorInterceptor implements HttpInterceptor {
   ) {}
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    if (req?.body?.refresh_token) {
+      return next.handle(req);
+    }
+
     return next.handle(req).pipe(
       catchError((err: HttpErrorResponse) => {
         if (err.status === 401) {
@@ -45,12 +49,7 @@ export class ErrorInterceptor implements HttpInterceptor {
             this.refreshTokenSubject.next(newAccessToken);
             this.store.dispatch(setAccessToken({ accessToken: newAccessToken }));
             this.store.dispatch(setRefreshToken({ refreshToken: response?.refresh_token }));
-            const newRequest = req.clone({
-              setHeaders: {
-                Authorization: `Bearer ${newAccessToken}`,
-              },
-            });
-            return next.handle(newRequest);
+            return next.handle(this.addToken(req, newAccessToken));
           }
 
           return throwError(() => new Error("Failed to refresh access token"));
@@ -70,19 +69,23 @@ export class ErrorInterceptor implements HttpInterceptor {
         filter((token) => token != null),
         take(1),
         switchMap((newAccessToken: string | null) => {
-          if (newAccessToken) {
-            const newRequest = req.clone({
-              setHeaders: {
-                Authorization: `Bearer ${newAccessToken}`,
-              },
-            });
-            return next.handle(newRequest);
-          }
-          return throwError(() => new Error("Failed to retry request after refresh"));
+          return next.handle(this.addToken(req, newAccessToken));
         }),
       );
     }
   }
+
+  private addToken(req: HttpRequest<any>, token: string | null): HttpRequest<any> {
+    if (token) {
+      return req.clone({
+        setHeaders: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+    }
+    return req;
+  }
+
   private handleRefreshTokenError(): void {
     this.store.dispatch(remove());
     this.router.navigate(["/auth/login"]);
