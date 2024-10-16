cbd4588afd341aaf9d26dda3d5deb1a16db15cc4
Farhan <farhaan.ranjha@gmail.com>
2024-10-16 16:08:28 +0500

    removed code from dashboard and moved to pending enrollments pages

--- src/app/pages/dashboard/dashboard.module.ts
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -1,22 +1,11 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
-import { ButtonModule } from "primeng/button";
-import { ProgressBarModule } from "primeng/progressbar";
 import { DashboardService } from "src/app/services/dashboard/dashboard.service";
-import { CustomFilterComponent } from "../../components/table/custom-filter/custom-filter.component";
-import { TableComponent } from "../../components/table/table.component";
 import { DashboardsRoutingModule } from "./dashboard-routing.module";
 import { DashboardComponent } from "./dashboard.component";
 
 @NgModule({
-  imports: [
-    CommonModule,
-    DashboardsRoutingModule,
-    TableComponent,
-    ButtonModule,
-    CustomFilterComponent,
-    ProgressBarModule,
-  ],
+  imports: [CommonModule, DashboardsRoutingModule],
   declarations: [DashboardComponent],
   providers: [DashboardService],
 })
